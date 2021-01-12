264d16c14da462578973ff7bc8d6957db81e4c6d
Hervé FRACKOWIAK <265578+Frackher@users.noreply.github.com>
2021-01-12 09:25:31 +0100

    First commit - polaris V1
    Lack of button to copy paste

---
+++ public/js/main.min.d3fd5986c0cd9a4eb99c826257a61ce801ecf0e13e23b8fdb835e3966b922a55.js
@@ -0,0 +1,2 @@
+function generate()
+{var name=[document.getElementById("in_name"),document.getElementById("out_name")];var func=[document.getElementById("in_func"),document.getElementById("out_func")];var email=[document.getElementById("in_email"),document.getElementById("out_email")];var tel=[document.getElementById("in_tel"),document.getElementById("out_tel")];name[1].textContent=name[0].value;func[1].textContent=func[0].value;email[1].textContent=email[0].value;tel[1].textContent=tel[0].value;}
